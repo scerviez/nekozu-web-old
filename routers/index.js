3d6c6c0c9b29cf3222f1c70a3a044de66fda8ee6
nekozu <81870902+nekozu@users.noreply.github.com>
2021-09-12 09:24:56 +0700

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/s', function (req, res) {
-    res.sendFile('index.html');
-});
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+app.use(express.static("express"));
+// default URL for website
+app.use('/s', function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
 
 app.post('/search', function (req, res) {
     var name = req.body.query + ' ';
