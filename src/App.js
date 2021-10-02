e1e6551e9206a9aa0bd865dabeb30037062d908d
nekozu <81870902+nekozu@users.noreply.github.com>
2021-10-02 21:20:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import './App.css';
 
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
       <h1>Welcome To Nekozu Website</h1>
