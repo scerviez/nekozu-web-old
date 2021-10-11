82343d98bb09038d4e4123deb24b36896c6933d4
nekozu <81870902+nekozu@users.noreply.github.com>
2021-10-11 18:23:00 +0700

    Create List.js

---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+ 
+export default class List extends Component {
+  render() {
+    return (
+      <div>
+        <h2>List Of Bots</h2>
+       <button class="button button1" onclick="window.location.href = 'https://t.me/akaragibot';">Akaragi Chat Bot</button>
+      </div>   
+      );
+  }
+}
