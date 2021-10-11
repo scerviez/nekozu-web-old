5356808b8fb9c1ddaaf75548b3dbb1e9291974b6
nekozu <81870902+nekozu@users.noreply.github.com>
2021-10-11 17:52:37 +0700

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+function Home() {
+  return (
+    <main>
+      <h1>Welcome To Nekozu Website</h1>
+      <h2>
+        See Our Channel At{' '}
+        <a
+          href="https://t.me/nekozu"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Telegram
+        </a>
+        !
+      </h2>
+      <p>
+        Nekozu Is A Channel At Telegram To Create A Multifunction Bot And Open Source
+        .
+      </p>
+      <br />
+      <footer>
+        <p>© 2021<a href="https://t.me/nekozu"> Nekozu</a></p>
+      </footer>
+    </main>
+  );
+}
+
+export default Home;
