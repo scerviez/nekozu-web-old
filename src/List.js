8db7a208f15eb7f860277f9e0040397d9cf2c011
nekozu <81870902+nekozu@users.noreply.github.com>
2021-10-11 19:17:04 +0700

    Update List.js

--- src/List.js
+++ src/List.js
@@ -6,6 +6,7 @@ export default class List extends Component {
       <div>
         <h2>List Of Bots</h2>
         <a href="https://t.me/akaragibot">Akaragi Chat Bot</a>
+        <a href="https://t.me/bruhulbott">Bruhul Bot</a>
       </div>   
       );
   }
